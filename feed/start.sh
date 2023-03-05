7ccd432ee669546213c42bdca01b5db8454c5bb6
jst2702 <jst2702@vt.edu>
2023-03-04 20:24:08 -0500

    .

--- feed/start.sh
+++ feed/start.sh
@@ -6,7 +6,13 @@ echo "run db migration"
 echo "before env file"
 echo $DB_SOURCE
 
-source app.env
+search_dir=/app
+for entry in "$search_dir"/*
+do
+  echo "$entry"
+done
+
+source /app/app.env
 echo "after env file"
 echo $DB_SOURCE
 /app/migrate -path /app/migrations -database "$DB_SOURCE" -verbose up
