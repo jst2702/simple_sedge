96dc3bb0a598c2a41d7bf912b0e933becd00b6a2
jst2702 <jst2702@vt.edu>
2023-03-06 21:44:58 -0500

    .

--- feed/start.sh
+++ feed/start.sh
@@ -5,7 +5,7 @@ set -e
 echo "run db migration"
 # source /app/app.env
 set -a
-. ./app/app.env
+. /app/app.env
 set +a
 echo $DB_SOURCE
 /app/migrate -path /app/migrations -database "$DB_SOURCE" -verbose up
